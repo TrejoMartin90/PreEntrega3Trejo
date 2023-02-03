ece37a758d4ceedc5bba913927da69f45f7b9f34
María Belén Monini <trejomartin90@gmail.com>
2023-02-03 16:41:32 -0300

    Agregando las views y las URLs para la vista y creacion de los objetos

---
+++ PreEntrega3Trejo/MyVet/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from MyVet import views
+
+urlpatterns = [
+    path('', views.home),
+    path('profesionales', views.ver_profesionales, name="Ver Profesionales"),
+    path('pacientes', views.ver_pacientes, name="Ver Pacientes"),
+    path('visitas', views.ver_visitas, name="Ver Visitas"),
+    path('crearProfesional', views.crear_profesional, name="Crear Profesionales"),
+    path('crearPaciente', views.crear_paciente, name="Crear Paciente"),
+    path('crearVisita', views.crear_visita, name="Crear Visita"),
+]
--- PreEntrega3Trejo/MyVet/views.py
+++ PreEntrega3Trejo/MyVet/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+	return HttpResponse('Home Page')
+
+def ver_profesionales (request):
+	return HttpResponse("Listado de Profesionales")
+
+def ver_visitas (request):
+	return HttpResponse("Listado de Visitas")
+
+def ver_pacientes (request):
+	return HttpResponse("Listado de Pacientes")
+
+def crear_profesional (request):
+	return HttpResponse("Creación de Profesional")
+
+def crear_visita (request):
+	return HttpResponse("Creación de Visitas")
+
+def crear_paciente (request):
+	return HttpResponse("Creación de Pacientes")
